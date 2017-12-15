db5a3bd05445f9f90ff8750bba650202e58fc55f
Sparshi Dhiman <sparshi.dhiman@srijan.net>
2017-12-15 17:54:28 +0530

    Adding slack.php11

--- slack.php
+++ slack.php
@@ -6,7 +6,7 @@ $COMMIT_REF = getenv('COMMIT_REF');
 $BUILD_ID = getenv('BUILD_ID');
 $SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
 
-$slack_message =  "Commit: https://github.com/sparshi/Behat_Probo/commit/${COMMIT_REF}\n".
+$slack_message =  "Commit: https://github.com/sparshi/Behat_Probo/commit/${COMMIT_REF}\n";
 // 		"Probo Build: https://app.probo.ci/#/dashboard/build/${BUILD_ID}\n".
 // 		"Commit author: ${commit_author}\n".
 // 		"Commit message: ```${commit_message}```\n".
