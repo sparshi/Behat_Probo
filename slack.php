feff84aebc29f89fb03aac894d17ac6e123cb0f3
Sparshi Dhiman <sparshi.dhiman@srijan.net>
2017-12-15 17:49:24 +0530

    Adding slack.php8

--- slack.php
+++ slack.php
@@ -1,11 +1,11 @@
 <?php
 
-// // COMMIT_REF is an environment variable set by probo
-// // see https://docs.probo.ci/build/steps/
-// $COMMIT_REF = getenv('COMMIT_REF');
-// $BUILD_ID = getenv('BUILD_ID');
-// $SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
-//
+// COMMIT_REF is an environment variable set by probo
+// see https://docs.probo.ci/build/steps/
+$COMMIT_REF = getenv('COMMIT_REF');
+$BUILD_ID = getenv('BUILD_ID');
+$SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+
 // $slack_message =  "Commit: https://github.com/sparshi/Behat_Probo/commit/${COMMIT_REF}\n".
 // 		"Probo Build: https://app.probo.ci/#/dashboard/build/${BUILD_ID}\n".
 // 		"Commit author: ${commit_author}\n".
