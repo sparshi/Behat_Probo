9a8a5462ea20ef7f1feaca5abb392a9bd328fbf5
Sparshi Dhiman <sparshi.dhiman@srijan.net>
2017-12-27 18:08:13 +0530

    New changes

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Wraith
-apt-get install ruby-full
-apt-get install imagemagick
+# apt-get install ruby-full
+# apt-get install imagemagick
+# gem install wraith -v 3.2.0
+
+apt-get -qq update
+apt-get -y install imagemagick
 gem install wraith -v 3.2.0
 
 # Versions
