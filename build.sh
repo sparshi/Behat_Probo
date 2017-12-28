045ba4f6c58301439bbf4b99ce65af82ec87c4f6
Sparshi Dhiman <sparshi.dhiman@srijan.net>
2017-12-28 11:43:30 +0530

    New changes

--- build.sh
+++ build.sh
@@ -9,15 +9,11 @@
 # apt-get -y install imagemagick
 # gem install wraith -v 3.2.0
 
-sudo apt-get -y update
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
-cd /tmp
-wget http://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p481.tar.gz
-tar -xvzf ruby-2.0.0-p481.tar.gz
-cd ruby-2.0.0-p481/
-./configure --prefix=/usr/local
-make
-sudo make install
+brew update
+brew install rbenv
+brew install ruby-build
+brew install openssl
+CONFIGURE_OPTS=--with-openssl-dir=`brew --prefix openssl` rbenv install 2.0.0-preview1
 
 # Versions
 # echo "wraith > " && wraith -v
